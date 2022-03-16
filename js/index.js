1affef4ab83eb5a0fb3a714230b02b1bfa70f0ba
Rose <rose.kamau@student.moringaschool.com>
2022-03-16 09:01:36 +0300

    add clickable function

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".clickable1").click(function() {
+        $(".design-desc").toggle();
+        $(".click-1").toggle();
+    });
+    $(".clickable2").click(function() {
+        $(".dev").toggle();
+        $(".click-2").toggle();
+    });
+    $(".clickable3").click(function() {
+        $(".product").toggle();
+        $(".click-3").toggle();
+    });
+  });
